61390aec23c750f9216cf1e126be72c5d778dae3
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2022-02-23 14:02:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = bracketsConfig.join("").replace(/,/g, "");
+  let stack = [];
+  for (let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+
+    if (bracketsIndex % 2 === 0) {
+      if (
+        bracket === brackets[bracketsIndex + 1] &&
+        stack[stack.length - 1] === bracketsIndex
+      ) {
+        stack.pop();
+      } else if (
+        bracket === brackets[bracketsIndex + 1] &&
+        stack[stack.length - 1] !== bracketsIndex
+      ) {
+        stack.push(bracketsIndex);
+      } else {
+        stack.push(bracketsIndex);
+      }
+    } else {
+      if (stack.pop() !== bracketsIndex - 1) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(check("()", [["(", ")"]])); // -> true
+console.log(check("((()))()", [["(", ")"]])); // -> true
+console.log(check("())(", [["(", ")"]])); // -> false
+console.log(
+  check("([{}])", [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+  ])
+); // -> true
+console.log(
+  check("[(])", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> false
+console.log(
+  check("[]()", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> true
+console.log(
+  check("[]()(", [
+    ["(", ")"],
+    ["[", "]"],
+  ])
+); // -> false
+
+// special case: opening and closing bracket can be the same :)
+
+console.log(check("||", [["|", "|"]])); // -> true
+console.log(
+  check("|()|", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> true
+console.log(
+  check("|(|)", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> false
+console.log(
+  check("|()|(||)||", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+); // -> true
